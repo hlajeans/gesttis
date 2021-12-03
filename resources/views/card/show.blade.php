d78dd56a44edee1b69e33f71e7318582a28c4275
hlajeans20 <helmuthmauricioalbaquiroz@gmail.com>
2021-12-03 14:47:29 -0400

    Add titulos

--- resources/views/card/show.blade.php
+++ resources/views/card/show.blade.php
@@ -1,3 +1,4 @@
+<title> {{$card->Titulo}} | Gestion TIS </title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <form action="{{url('/card/'.$card->id)}}" method="post" enctype="multipart/form-data">
 @csrf
