3a0ed252221ade36198e13a21577a1b06ac107d5
hlajeans20 <helmuthmauricioalbaquiroz@gmail.com>
2021-12-10 15:08:23 -0400

    Descarga arreglada

--- resources/views/card/show.blade.php
+++ resources/views/card/show.blade.php
@@ -17,7 +17,7 @@
         <ul>
           <li><strong></strong>{{$card->Descripcion}}</li>
           <br/>
-            <a href="/storage/app/public/uploads/.{{$card->Archivo}}" download class="btn btn-warning">Descargar archivos adjuntos</a>
+            <a href="{{asset('storage').'/'.$card->Archivo}}" download class="btn btn-warning">Descargar archivos adjuntos</a>
           <a href="{{url('/card')}}" class="btn btn-dark">Regresar</a>
         <br/>
         </ul>
