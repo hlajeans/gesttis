b20cecbc7c0f3a65d6be2b81242952e42b18db34
hlajeans20 <helmuthmauricioalbaquiroz@gmail.com>
2022-01-14 00:27:42 -0400

    fix gp

--- resources/views/grupoempresa/index.blade.php
+++ resources/views/grupoempresa/index.blade.php
@@ -31,9 +31,7 @@
 <br/>
 <br/>
 <div class="container">
-    @if(Auth::user()->rol==2 | Auth::user()->rol==3| Auth::user()->rol==1)
 <a href="{{route('card.index')}}" class="btn btn-dark">Mi Grupo Empresa</a>
-@endif
 @if(Auth::user()->rol==1 | Auth::user()->rol==2| Auth::user()->rol==3)
 <a href="{{url('/grupoempresa/create')}}" class="btn btn-dark">Registrar Grupo Empresa</a>
 @endif
